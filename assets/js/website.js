1b3a93e8526a0d3f5f4160ccacc0c50f9e3308c2
jonsmith1982 <simon.hodgson.1982@gmail.com>
2020-12-04 23:54:40 +0000

    Create historical stats per core in sessionStorage

--- assets/js/website.js
+++ assets/js/website.js
@@ -2,6 +2,8 @@
 var cpuStats = require('./assets/js/cpu_stats.js');
 var cpuCores = cpuStats.totalCores();
 
+sessionStorage = window.sessionStorage;
+
 function cpuUsage(err, percent, seconds, coreIndex) {
   if (err) {
     return console.log(err);
@@ -9,6 +11,11 @@ function cpuUsage(err, percent, seconds, coreIndex) {
   
   // Store data for each cpu here so we can show it in a large line graph 
   let cpuPercent = Math.ceil(percent);
+  let coreStats = sessionStorage.getItem("cpu_stats_" + coreIndex);
+  coreStats = !Array.isArray(coreStats) ? [coreStats] : coreStats;
+  // Find out how many stats to hold for each core and clean up excess data as we go along.
+  coreStats.push(cpuPercent);
+  sessionStorage.setItem("cpu_stats_" + coreIndex, coreStats);
   $(".gauge_" + coreIndex + " .value").text(cpuPercent + "%");
   $(".gauge_" + coreIndex).css({ "--rotation": 180 * (cpuPercent / 100) + "deg"});
   cpuStats.usagePercent({coreIndex: coreIndex, sampleMs: 1000}, cpuUsage);
