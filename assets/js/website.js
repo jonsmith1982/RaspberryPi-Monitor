5336121b8c3c0aa128b6a84cc099e7b54637cab7
jonsmith1982 <simon.hodgson.1982@gmail.com>
2020-12-04 23:01:40 +0000

    Tidied up the cpu stats code

--- assets/js/website.js
+++ assets/js/website.js
@@ -6,24 +6,20 @@ function cpuUsage(err, percent, seconds, coreIndex) {
   if (err) {
     return console.log(err);
   }
-
-  //console.log(coreIndex);
-  //console.log(percent);
-  //console.log(seconds);
+  
+  // Store data for each cpu here so we can show it in a large line graph 
   let cpuPercent = Math.ceil(percent);
   $(".gauge_" + coreIndex + " .value").text(cpuPercent + "%");
-  $(".gauge_" + coreIndex).css({ "--rotation": 160 * (cpuPercent / 100) + "deg"});
+  $(".gauge_" + coreIndex).css({ "--rotation": 180 * (cpuPercent / 100) + "deg"});
   cpuStats.usagePercent({coreIndex: coreIndex, sampleMs: 2000}, cpuUsage);
 }
 
 $(document).ready(function() {
-  //console.log( "ready!" );
     
   for (const x of Array(cpuCores).keys()) {
     $(".gauge.template").clone().appendTo("#cpu_stats");
     $("#cpu_stats .gauge:last-child").removeClass("template");
-    $("#cpu_stats .gauge:last-child").addClass("gauge_" + x);
-//     $("#cpu_stats .gauge:last-child").addClass("col-sm");
+    $("#cpu_stats .gauge:last-child").addClass("gauge_" + x + " col-xs");
     $("#cpu_stats .gauge:last-child").show();
     cpuStats.usagePercent({coreIndex: x, sampleMs: 1000}, cpuUsage);
   }
