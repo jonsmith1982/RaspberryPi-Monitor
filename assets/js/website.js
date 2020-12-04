3ca5bfb26ab6ce37160503057875430bd7260a97
jonsmith1982 <simon.hodgson.1982@gmail.com>
2020-12-04 20:21:54 +0000

    The gauges now update automatically

--- assets/js/website.js
+++ assets/js/website.js
@@ -7,14 +7,14 @@ function cpuUsage(err, percent, seconds, coreIndex) {
     return console.log(err);
   }
 
-  console.log(coreIndex);
-  console.log(percent);
-  console.log(seconds);
-  $(".gauge_" + coreIndex + " .value").text(Math.ceil(percent) + "%");
+  //console.log(coreIndex);
+  //console.log(percent);
+  //console.log(seconds);
+  let cpuPercent = Math.ceil(percent);
+  $(".gauge_" + coreIndex + " .value").text(cpuPercent + "%");
+  $(".gauge_" + coreIndex).css({ "--rotation": 160 * (cpuPercent / 100) + "deg"});
   cpuStats.usagePercent({coreIndex: coreIndex, sampleMs: 2000}, cpuUsage);
 }
-
-
 
 $(document).ready(function() {
   //console.log( "ready!" );
@@ -25,7 +25,7 @@ $(document).ready(function() {
     $("#cpu_stats .gauge:last-child").addClass("gauge_" + x);
 //     $("#cpu_stats .gauge:last-child").addClass("col-sm");
     $("#cpu_stats .gauge:last-child").show();
-    cpuStats.usagePercent({coreIndex: x, sampleMs: 2000}, cpuUsage);
+    cpuStats.usagePercent({coreIndex: x, sampleMs: 1000}, cpuUsage);
   }
     
 });
