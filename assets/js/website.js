5e0faf5ebb712091e53cc30e73d0b4c0b3b116dd
jonsmith1982 <simon.hodgson.1982@gmail.com>
2020-12-05 00:17:03 +0000

    Tidying array of core stats up, keeping them down in length

--- assets/js/website.js
+++ assets/js/website.js
@@ -11,14 +11,17 @@ function cpuUsage(err, percent, seconds, coreIndex) {
   
   // Store data for each cpu here so we can show it in a large line graph 
   let cpuPercent = Math.ceil(percent);
-  let coreStats = sessionStorage.getItem("cpu_stats_" + coreIndex);
-  coreStats = !Array.isArray(coreStats) ? [coreStats] : coreStats;
+  let coreStats = sessionStorage.getItem("cpu_stats_" + coreIndex) ? 
+    sessionStorage.getItem("cpu_stats_" + coreIndex) : '';
+  coreStats = !Array.isArray(coreStats) ? coreStats.split(',') : coreStats;
   // Find out how many stats to hold for each core and clean up excess data as we go along.
   coreStats.push(cpuPercent);
+  if (coreStats.length >= 10)
+    coreStats.shift();
   sessionStorage.setItem("cpu_stats_" + coreIndex, coreStats);
   $(".gauge_" + coreIndex + " .value").text(cpuPercent + "%");
   $(".gauge_" + coreIndex).css({ "--rotation": 180 * (cpuPercent / 100) + "deg"});
-  cpuStats.usagePercent({coreIndex: coreIndex, sampleMs: 1000}, cpuUsage);
+ cpuStats.usagePercent({coreIndex: coreIndex, sampleMs: 1000}, cpuUsage);
 }
 
 $(document).ready(function() {
