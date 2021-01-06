fee8ef4febb15ca4850d0aa8f2131bd1c452a899
Simon Hodgson <simon.hodgson.1982@gmail.com>
2021-01-06 11:57:44 +0000

    Updated styles, graphs and layout of Overview

--- assets/js/website.js
+++ assets/js/website.js
@@ -21,10 +21,11 @@ function processStorage(key, value) {
 }
 
 const ceil = (integer) => {return Math.ceil(integer)}
+const round = (integer) => {return Math.round(integer)}
 
 var cpuGaugeOptions = {
-  width: 140,
-  height: 140,
+  width: 75,
+  height: 75,
   units: null,
   startAngle: 60,
   ticksAngle: 240,
@@ -58,53 +59,29 @@ function cpuUsage(err, percent, seconds, coreIndex) {
   }
   let cpuPercent = Math.ceil(percent);
   processStorage('cpu_stats_' + coreIndex, cpuPercent);
-  document.gauges[coreIndex].value = cpuPercent;
+  document.gauges[coreIndex].value = percent;
   cpuStats.usagePercent({coreIndex: coreIndex, sampleMs: timeOut}, cpuUsage);
 }
 
 function memUsage(previousPercent = null) {
   let memStatsAll = memStats.allStats("GiB");
   let usedPercent = ceil(memStatsAll.usedPercent);
+  let usedGB = round((memStatsAll.total * (memStatsAll.usedPercent / 100)) * 100) / 100;
+  let freeGB = round(memStatsAll.free * 100) / 100;
+  let totalGB = round(memStatsAll.total * 100) / 100;
+  $("#mem_label").html("<strong>Used:</strong> " + usedGB + "GiB <strong>Available:</strong> " + freeGB + "GiB of " + totalGB + "GiB");
+  $("#swap_label").html("<strong>Used:</strong> " + usedGB + "GiB <strong>Available:</strong> " + freeGB + "GiB of " + totalGB + "GiB");
   processStorage('mem_stats', usedPercent);
   if (previousPercent === null || previousPercent !== usedPercent) {
-    $("meter#memory").val(usedPercent);
+    $("#mem_gauge").css("width", usedPercent + "%");
+    $("#mem_gauge").text(usedPercent + "%");
+    $("#mem_gauge").attr("aria-valuenow", usedPercent);
+    $("#swap_gauge").css("width", usedPercent + "%");
+    $("#swap_gauge").text(usedPercent + "%");
+    $("#swap_gauge").attr("aria-valuenow", usedPercent);
   }
   setTimeout(memUsage, timeOut, usedPercent);
 }
-
-var tempGaugeOptions = {
-    renderTo: 'temp_gauge',
-    width: 100,
-    height: 130,
-    minValue: 0,
-    startAngle: 90,
-    ticksAngle: 180,
-    valueBox: false,
-    maxValue: 80,
-    majorTicks: ["0", "40", "80"],
-    minorTicks: 2,
-    strokeTicks: true,
-    highlights: [],
-    colorPlate: "transparent",
-    colorBarEnd: '#aaa',
-    colorBarStroke: '#eee',
-    colorMajorTicks: '#666',
-    colorMinorTicks: '#666',
-    colorNumbers: '#666',
-    borderShadowWidth: 12,
-    borders: true,
-    needleType: "arrow",
-    needleWidth: 2,
-    animationDuration: 500,
-    animationRule: "quad",
-    barWidth: 17,
-    barStrokeWidth:/* */1,
-    colorBarProgress: 'rgba(50,50,200,0.75)',
-    tickSide: "left",
-    numberSide: "left",
-    needleSide: "left",
-    value: 35
-};
 
 function cpuTemperature() {
   fs.readFile('/sys/class/thermal/thermal_zone0/temp', 'utf8' , (err, data) => {
@@ -126,13 +103,20 @@ $(document).ready(function() {
     gaugeOptions = cpuGaugeOptions;
     gaugeOptions.title = title;
     gaugeOptions.renderTo = renderTo;
-    $('#cpu_graphs').append('<canvas id="' + renderTo + '"></canvas>');
+    $('#cpu_graphs').append('<div class="col-3 col-md-6 col-lg-3 text-center"><canvas id="' + renderTo + '" ></canvas></div>');
     new gauges.RadialGauge(gaugeOptions).draw(); 
     cpuStats.usagePercent({coreIndex: x, sampleMs: timeOut}, cpuUsage);
   }
   
-  $("#cpu_graphs").append('<canvas id="temp_gauge"></canvas>');
-  new gauges.LinearGauge(tempGaugeOptions).draw();
+  $("#temp_graphs").append('<canvas id="temp_gauge"></canvas>');
+  gaugeOptions = cpuGaugeOptions;
+  gaugeOptions.width = 120;
+  gaugeOptions.height = 120;
+  gaugeOptions.maxValue = 80;
+  gaugeOptions.title = 'CPU Temp';
+  gaugeOptions.renderTo = 'temp_gauge';
+  gaugeOptions.colorBarProgress = 'rgba(255,0,0,.5)';
+  new gauges.RadialGauge(gaugeOptions).draw(); 
   cpuTemperature();
   
   memUsage();
