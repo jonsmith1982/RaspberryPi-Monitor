b9b1775a34d62df2fcf002718dcfb9ca6a067144
Simon Hodgson <simon.hodgson.1982@gmail.com>
2021-01-13 20:00:26 +0000

    Made navbar list dynamic - may need a more complete templating system?

--- assets/js/website.js
+++ assets/js/website.js
@@ -1,3 +1,15 @@
+function navbar(page) {
+  let ul = '';
+  const pages = ['overview', 'statistics'];
+  for (const x of pages) {
+    const active = x === page ? ' active' : '';
+    const sr = x === page ? ' <span class="sr-only">(current)</span>' : '';
+    const li = '<li class="nav-item' + active + '"><a class="nav-link" href="./' + x + '.html">' + x.charAt(0).toUpperCase() + x.slice(1) + sr + '</a></li>';
+    ul += li;
+  }
+  return(ul);
+}
+
 $(document).ready(function() {
-  
+  $(".navbar-nav").html(navbar(page));
 });
