ced47377c0c3c748909873ab7fea2ab8c42f9d6f
Simon Hodgson <simon.hodgson.1982@gmail.com>
2021-01-15 17:18:42 +0000

    Included flot basic usage example into statistics page

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -2,5 +2,11 @@ const piMonitor = require('../assets/js/raspberrypi_monitor.js');
 const cpuCores = piMonitor.totalCores();
 
 $(document).ready(function() {
-  
+  var d1 = [];
+  for (var i = 0; i < 14; i += 0.5) {
+    d1.push([i, Math.sin(i)]);
+  }
+  var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+  var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+  $.plot("#placeholder", [ d1, d2, d3 ]);
 });
