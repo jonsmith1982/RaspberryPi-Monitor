6bcd230b43fec6e483fe48c1e1184565dcbf3109
Simon Hodgson <simon.hodgson.1982@gmail.com>
2021-01-16 22:41:28 +0000

    Added options to graphs to improve presentation

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -19,8 +19,39 @@ function memGraphData() {
   return(data);
 }
 
+let options = {
+  yaxis: {
+    autoScale: "none",
+    min: 0,
+    max: 100,
+    tickDecimals: 0,
+    font: {
+      size: 11,
+      lineHeight: 13,
+      style: "italic",
+      weight: "bold",
+      family: "sans-serif",
+      variant: "small-caps",
+      color: "#545454"
+    }
+  },
+  xaxis: {
+    show: true
+  },
+  series: {
+    lines: {
+      show: true,
+      lineWidth: 2,
+      fill: false
+    },
+    points: {
+      show: false
+    }
+  }
+};
+
 $(document).ready(function() {
-  $.plot("#cpu_usage_graph", cpuGraphData());
-  $.plot("#cpu_temp_graph", tempGraphData());
-  $.plot("#mem_usage_graph", memGraphData());
+  $.plot("#cpu_usage_graph", cpuGraphData(), options);
+  $.plot("#cpu_temp_graph", tempGraphData(), options);
+  $.plot("#mem_usage_graph", memGraphData(), options);
 });
