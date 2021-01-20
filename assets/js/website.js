0558200ae03d713a1cf9b50ab1cb0e13d2a5b52f
Simon Hodgson <simon.hodgson.1982@gmail.com>
2021-01-20 21:38:43 +0000

    Created settings modal and appended it to the html body

--- assets/js/website.js
+++ assets/js/website.js
@@ -15,6 +15,12 @@ function navigation(page) {
   return(nav);
 }
 
+function settingsModal() {
+  const modal = '<div class="modal fade" id="settings-modal" tabindex="-1" aria-labelledby="settings-modal-label" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="settings-modal-label">Settings</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body">...</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button></div></div></div></div>';
+  return(modal);
+}
+
 $(document).ready(function() {
   $("#content").prepend(navigation(page));
+  $('body').append(settingsModal());
 });
