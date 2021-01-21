fb7e4b8d5a2a29dd26fa1b30ac8109fcf2af1a26
Simon Hodgson <simon.hodgson.1982@gmail.com>
2021-01-21 17:18:20 +0000

    Checked switch for each section according to settings hash

--- assets/js/website.js
+++ assets/js/website.js
@@ -19,7 +19,8 @@ function navigation(page) {
 function settingsModal(page) {
   let li = [];
   for (const x of Object.keys(settings[page])) {
-    li.push('<li class="list-group-item"><div class="custom-control custom-switch"><input type="checkbox" class="custom-control-input" id="settings_' + x + '" checked="checked"><label class="custom-control-label" for="settings_' + x + '">' + settings[page][x].label + '</label></div></li>');
+    const checked = settings[page][x].status ? ' checked="checked"' : '';
+    li.push('<li class="list-group-item"><div class="custom-control custom-switch"><input type="checkbox" class="custom-control-input" id="settings_' + x + '"' + checked + '><label class="custom-control-label" for="settings_' + x + '">' + settings[page][x].label + '</label></div></li>');
   }
   const modal = '<div class="modal fade" id="settings-modal" tabindex="-1" aria-labelledby="settings-modal-label" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="settings-modal-label">Settings</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><ul class="list-group">' + li.join('') + '</ul></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button></div></div></div></div>';
   return(modal);
