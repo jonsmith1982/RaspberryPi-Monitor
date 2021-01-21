bbca6b1af0be202ede9bd79a606ab3bd3877bf6d
Simon Hodgson <simon.hodgson.1982@gmail.com>
2021-01-21 16:03:13 +0000

    Changed functions to use settings hash instead of temporary arrays

--- assets/js/website.js
+++ assets/js/website.js
@@ -1,11 +1,8 @@
-const sections = {version: 'Version Information', cpu: 'CPU Gauges', harddisks: 'Hard Disk Details', uptime: 'Uptime Information', temperature: 'Temperature Gauge', memory: 'Memory Meter', swap: 'Swap Meter', wifi: 'Wifi Details', network: 'Network Interfaces', partitions: 'Partitions Information'};
-
 const settings = {overview: {version: {label: 'Version Information', title: 'Version', status: true}, cpu: {label: 'CPU Gauges', title: 'CPU', status: true}, harddisks: {label: 'Hard Disk Details', title: 'Hard Disks', status: true}, uptime: {label: 'Uptime Information', title: 'Uptime', status: true}, temperature: {label: 'Temperature Gauge', title: 'Temperature', status: true}, memory: {label: 'Memory Meter', title: 'Memory', status: true}, swap: {label: 'Swap Meter', title: 'Swap', status: true}, wifi: {label: 'Wifi Details', title: 'Wifi', status: true}, network: {label: 'Network Interfaces', title: 'Network', status: true}, partitions: {label: 'Partitions Information', title: 'Partitions', status: true}}, statistics: {cpu: {label: 'CPU Graph', title: 'CPU', status: true}, temperature: {label: 'Temperature Graph', title: 'Temperature', status: true}, memory: {label: 'Memory Graph', title: 'Memory', status: true}}};
 
 function navbar(page) {
   let ul = '';
-  const pages = ['overview', 'statistics'];
-  for (const x of pages) {
+  for (const x of Object.keys(settings)) {
     const active = x === page ? ' active' : '';
     const sr = x === page ? ' <span class="sr-only">(current)</span>' : '';
     const li = '<li class="nav-item' + active + '"><a class="nav-link" href="./' + x + '.html">' + x.charAt(0).toUpperCase() + x.slice(1) + sr + '</a></li>';
@@ -19,10 +16,10 @@ function navigation(page) {
   return(nav);
 }
 
-function settingsModal() {
+function settingsModal(page) {
   let li = [];
-  for (const x of Object.keys(sections)) {
-    li.push('<li class="list-group-item"><div class="custom-control custom-switch"><input type="checkbox" class="custom-control-input" id="settings_' + x + '" checked="checked"><label class="custom-control-label" for="settings_' + x + '">' + sections[x] + '</label></div></li>');
+  for (const x of Object.keys(settings[page])) {
+    li.push('<li class="list-group-item"><div class="custom-control custom-switch"><input type="checkbox" class="custom-control-input" id="settings_' + x + '" checked="checked"><label class="custom-control-label" for="settings_' + x + '">' + settings[page][x].label + '</label></div></li>');
   }
   const modal = '<div class="modal fade" id="settings-modal" tabindex="-1" aria-labelledby="settings-modal-label" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="settings-modal-label">Settings</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><ul class="list-group">' + li.join('') + '</ul></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button></div></div></div></div>';
   return(modal);
@@ -30,5 +27,5 @@ function settingsModal() {
 
 $(document).ready(function() {
   $("#content").prepend(navigation(page));
-  $('body').append(settingsModal());
+  $('body').append(settingsModal(page));
 });
